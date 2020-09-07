3f660494b23a8dadd924234c53d20d5a95ad68f6
Aiyyskhan <aiyyskhan@gmail.com>
2020-09-07 18:09:17 +0900

    отредактированы main.py и index.html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 server = Flask(__name__)
 
 @server.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     server.run(debug=True)
