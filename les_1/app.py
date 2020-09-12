9904197257242a085f5d45bab90bc03ccce9b2a1
Aiyyskhan <aiyyskhan@gmail.com>
2020-09-12 13:44:01 +0900

    изменена структура проекта

---
+++ les_1/app.py
@@ -0,0 +1,34 @@
+import json
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello():
+#     return "Hello, Flask!"
+
+
+@app.route("/")
+def home():
+    return render_template(
+        "page_1.html"
+    )
+
+
+@app.route("/test")
+def test():
+    # for example: http://localhost:5000/test?name=Ivan&lastname=Ivanov
+    data = [] #'100', '200', '300', '400']
+    return render_template(
+        "test.html", 
+        name=request.args["name"], 
+        lastname=request.args["lastname"],
+        data=data
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
