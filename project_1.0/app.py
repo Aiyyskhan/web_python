209382ec0de45551fa370b6e40b11c3b504dd769
Aiyyskhan <aiyyskhan@gmail.com>
2020-12-15 17:52:14 +0900

    modified project_1.0

--- project_1.0/app.py
+++ project_1.0/app.py
@@ -11,5 +11,17 @@ def index_page():
         the_title="My Site"
     )
 
+@app.route('/product')
+def product_page():
+    return render_template(
+        "product.html"
+    )
+
+@app.route('/contact')
+def contact_page():
+    return render_template(
+        "contact.html"
+    )
+
 if __name__ == "__main__":
     app.run(debug=True)
